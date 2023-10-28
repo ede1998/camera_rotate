7bf51d2a9f23897f56eb0cc687857491afd643ae
Erik Hennig <online@erik-hennig.me>
2023-10-28 21:18:50 +0200

    add image size/depth analytics, reformat

--- vitis/camera_rotate/src/host.cpp
+++ vitis/camera_rotate/src/host.cpp
@@ -3,42 +3,42 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 #include <stdio.h>
-using namespace cv;
-using namespace std;
-int main(int, char**)
-{
- Mat frame;
- //--- INITIALIZE VIDEOCAPTURE
- VideoCapture cap;
- // open the default camera using default API
- // cap.open(0);
- // OR advance usage: select any API backend
- int deviceID = 0; // 0 = open default camera
- int apiID = cv::CAP_ANY; // 0 = autodetect default API
- // open selected camera using selected API
- cap.open(deviceID, apiID);
- // check if we succeeded
- if (!cap.isOpened()) {
- cerr << "ERROR! Unable to open camera\n";
- return -1;
- }
- //--- GRAB AND WRITE LOOP
- cout << "Start grabbing" << endl
- << "Press any key to terminate" << endl;
- for (;;)
- {
- // wait for a new frame from camera and store it into 'frame'
- cap.read(frame);
- // check if we succeeded
- if (frame.empty()) {
- cerr << "ERROR! blank frame grabbed\n";
- break;
- }
- // show live and wait for a key with timeout long enough to show images
- imshow("Live", frame);
- if (waitKey(5) >= 0)
- break;
- }
- // the camera will be deinitialized automatically in VideoCapture destructor
- return 0;
+
+int main(int, char**) {
+	cv::Mat frame;
+	//--- INITIALIZE VIDEOCAPTURE
+	cv::VideoCapture cap;
+	// open the default camera using default API
+	// cap.open(0);
+	// OR advance usage: select any API backend
+	int deviceID = 0; // 0 = open default camera
+	int apiID = cv::CAP_ANY; // 0 = autodetect default API
+	// open selected camera using selected API
+	cap.open(deviceID, apiID);
+	// check if we succeeded
+	if (!cap.isOpened()) {
+		std::cerr << "ERROR! Unable to open camera\n";
+		return -1;
+	}
+	//--- GRAB AND WRITE LOOP
+	std::cout << "Start grabbing" << std::endl << "Press any key to terminate"
+			<< std::endl;
+	for (;;) {
+		// wait for a new frame from camera and store it into 'frame' and check if we succeeded
+		if (!cap.read(frame)) {
+			std::cerr << "ERROR! blank frame grabbed\n";
+			break;
+		}
+		std::cout << "Image size: " << frame.cols << "x" << frame.rows
+				<< ", depth: " << frame.depth() << ", channels: "
+				<< frame.channels() << std::endl;
+		// 1920x1080, CV_8U, 3
+		// show live and wait for a key with timeout long enough to show images
+		cv::imshow("Live", frame);
+		if (cv::waitKey(5) >= 0) {
+			break;
+		}
+	}
+	// the camera will be deinitialized automatically in VideoCapture destructor
+	return 0;
 }
