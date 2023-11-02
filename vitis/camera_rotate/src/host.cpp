2959a023b14874d73f50d24b70125a51226565ac
Erik Hennig <online@erik-hennig.me>
2023-11-02 22:12:18 +0100

    implement host.cpp to rotate image

--- vitis/camera_rotate/src/host.cpp
+++ vitis/camera_rotate/src/host.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <chrono>
 
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+
 // XRT includes
 #include "xrt/xrt_bo.h"
 #include "xrt/xrt_device.h"
@@ -14,111 +18,73 @@
 		- start_time).count();
 
 int main(int argc, char** argv) {
-	// Check the command line args
 	if (argc < 3) {
 	    std::cerr << "usage: camera_rotate <xclbin-file> <img>\n";
 	    return EXIT_FAILURE;
 	}
 
-	// Define variables for time measurement
-	auto start_time = std::chrono::high_resolution_clock::now();
-	auto end_time = std::chrono::high_resolution_clock::now();
+	std::chrono::high_resolution_clock::time_point start_time, end_time;
 
-	//Define binary file and device index
     std::string binaryFile = argv[1];
     int device_index = 0; //Device index should be 0 on the Kria board
 
-    // Define data size from args and vector size for buffers in byte
-    int data_size = std::stoi(argv[2]);
-    size_t vector_size_bytes = sizeof(int) * data_size;
-
-    int error_flag = 0; //Error flag
-
     std::cout << "-------------------------------------------------------------------"<<std::endl;
-    std::cout << "-- vadd host program                                             --"<<std::endl;
+    std::cout << "-- camera_rotate host program                                    --"<<std::endl;
     std::cout << "-------------------------------------------------------------------"<<std::endl;
 
-    //Open device
     auto device = xrt::device(device_index);
 
-    //Get the kernel object from xclbin:
-    //Load the xclbin file and get UUID.
-    //The UUID is needed to open the kernel from the device.
     std::cout << "Load the xclbin: " << binaryFile << std::endl;
-    auto uuid = device.load_xclbin(binaryFile);
-    // Get some information on the device
+    const auto uuid = device.load_xclbin(binaryFile);
     std::cout << "Device name: " << device.get_info<xrt::info::device::name>() << "\n";
-    //Then get the kernel object from the UUID, "krnl_vadd" is the name of the kernel
     auto krnl = xrt::kernel(device, uuid, "krnl_vadd");
+
+    const auto image = cv::imread(argv[2], cv::IMREAD_GRAYSCALE);
+	assert(image.rows > 10);
+	assert(image.cols > 10);
+	assert(image.type() == CV_8UC1);
+	assert(image.channels() == 1);
+	assert(image.isContinuous());
+
+	const auto image_size = image.total() * image.elemSize();
 
     // Allocate buffers for the kernel arguments with master interfaces in the
     // same memory bank as the kernel interfaces group id.
     // The group_id is the argument index of the HLS function.
     // The kernel has the following arguments (see HLS):
     // void krnl_vadd(Pixel *src_ptr, Pixel *dst_ptr, uint16_t rows, uint16_t cols,	uint8_t direction);
-    // in1, in2 and out are master interfaces with memory buffers to be allocated.
-    // size is a register in the kernel (slave interface) and needs no buffer.
-    auto bo0 = xrt::bo(device, vector_size_bytes, krnl.group_id(0));
-    auto bo1 = xrt::bo(device, vector_size_bytes, krnl.group_id(1));
-    auto bo_out = xrt::bo(device, vector_size_bytes, krnl.group_id(2));
+    // src_ptr and dest_ptr are master interfaces with memory buffers to be allocated.
+    // Others are registers in the kernel (slave interface) and needs no buffer.
+    auto src = xrt::bo(device, image_size, krnl.group_id(0));
+    auto dest = xrt::bo(device, image_size, krnl.group_id(1));
 
-    // Do the data transfer between host and device by the buffer map API:
-    // Map the host side backing pointer of the buffer to the user pointers,
-    // then use the user pointers to fill the buffers with test data.
-    auto bo0_map = bo0.map<int*>();
-    auto bo1_map = bo1.map<int*>();
-    auto bo_out_map = bo_out.map<int*>();
-    // Create the test data and reference data
-    int *bufReference = new int[data_size];
-    for (int i = 0; i < data_size; ++i) {
-        bo0_map[i] = i;
-        bo1_map[i] = i;
-        bo_out_map[i] = 0; //Fill output buffer with 0s
-        bufReference[i] = bo0_map[i] + bo1_map[i];
-    }
+    src.write(image.data);
 
     // Transfer the host data to the device using the sync API
     START_MEASUREMENT
-    bo0.sync(XCL_BO_SYNC_BO_TO_DEVICE);
-    bo1.sync(XCL_BO_SYNC_BO_TO_DEVICE);
+    src.sync(XCL_BO_SYNC_BO_TO_DEVICE);
     END_MEASUREMENT(duration_trf1)
 
-    //Execute the kernel: The signature (arguments) of the kernel execution
-    //corresponds to the signature of the HLS code of the kernel.
-    //The arguments 0-2 are the memory accesses via the buffers,
-    //the argument 3 is a scalar argument (slave register).
-	START_MEASUREMENT
-    auto run = krnl(bo0, bo1, bo_out, data_size); //Start the kernel
-    run.wait(); //Wait for completion of the kernel
-    //Calculate execution time
+  	START_MEASUREMENT
+    auto run = krnl(src, dest, image.rows, image.cols, 90);
+    run.wait();
     END_MEASUREMENT(duration_kernel)
 
     // Transfer device data to host using the sync API
 	START_MEASUREMENT
-    bo_out.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+    dest.sync(XCL_BO_SYNC_BO_FROM_DEVICE);
     END_MEASUREMENT(duration_trf2)
 
-    // Compare device output data with the reference data
-    for (int i = 0; i < data_size; ++i) {
-    	if(bo_out_map[i] != bufReference[i]){
-    		error_flag = 1;
-    	}
-    }
-
-    // Final verdict
-    std::cout << "-------------------------------------------------------------------"<<std::endl;
-    if(error_flag == 1)
-    	std::cout << "Test FAILED!" << std::endl;
-    else
-    	std::cout << "Test PASSED!" << std::endl;
-
+    const auto rotated_image = cv::Mat(image.rows, image.cols, image.type(), dest.map());
     //Get some info on execution times
     std::cout << "-------------------------------------------------------------------"<<std::endl;
-    std::cout << "Data size transferred (array size): " << data_size << std::endl;
+    std::cout << "Data size transferred (array size): " << image_size << std::endl;
 	std::cout << "Execution time kernel: " << duration_kernel << " us" << std::endl;
 	std::cout << "Transfer time to/from kernel memory: " << duration_trf1 << " us + " << duration_trf2
 			<< " us =  "<< duration_trf1+duration_trf2 << " us" << std::endl;
     std::cout << "-------------------------------------------------------------------"<<std::endl;
 
+    cv::imshow("Rotated", rotated_image);
+    cv::waitKey(0);
     return 0;
 }
